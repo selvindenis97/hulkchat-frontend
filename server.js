23b4f4fcdd3fa875ce0fc921cc29197e9615b706
selvin <stambolic@otys.nl>
2024-05-07 22:00:43 +0200

    updated package.json

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
+import express from 'express';
+import serveStatic from 'serve-static';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import process from 'process';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
